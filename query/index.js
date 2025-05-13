a505d07a291f9d94fc7b0e2e7deb40600efddb10
ets-gopi <gopi.m@entitledarts.com>
2025-05-13 19:27:21 +0530

    changes for kubernetes

--- query/index.js
+++ query/index.js
@@ -46,10 +46,9 @@ app.post("/events", (req, res) => {
 
 app.listen(4002, async () => {
   console.log("Listening on 4002");
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get("http://eventbus-srv:4005/events");
   for (let eve of res.data) {
     console.log("Processing Events", eve.type);
-
     handleEvent(eve.type, eve.data);
   }
 });
